51538ab6f77ce88dcd453580b5d9b1f43baa7191
peterirani <peshotanirani@gmail.com>
2020-05-25 19:11:49 -0700

    second commit

--- scripts-to-auto-generate-pandas-profiles/script.py
+++ scripts-to-auto-generate-pandas-profiles/script.py
@@ -3,4 +3,4 @@ import numpy as np
 from pandas_profiling import ProfileReport
 df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv", error_bad_lines=False)
 profile = ProfileReport(df, title='Pandas Profiling Report', explorative=True)
-profile.to_file("your_report.html")
+profile.to_file("../webpages/your_report.html")
